2ff1216c68c02b7c3d482d9ed17ec3a2d0328107
Dengchol2025 <dengcholkon2024@gmail.com>
2026-05-31 10:55:53 +0200

    Add Function.h with library management functions
    Roll 202511436

---
+++ Function.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "LibraryBook.h"
+
+// External function declarations
+void addBook();
+void displayBooks();
+void searchBook();
+void borrowBookMenu();
+void returnBookMenu();
+void displayOverdueBooks();
+void calculateTotalFines();
+void initializeLibrary();
+
+#endif // FUNCTIONS_H
